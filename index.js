753dd4bd7126ae27e879fa6aa6b1a88c7191e02f
Aron Rohr <aron.rohr@web.de>
2023-03-24 14:21:44 +0100

    updated code

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const csv = require('csv-parser');
-const discord_rpc = require('discord-rich-presence')('924324822421471242');
+const discord_rpc = require('discord-rich-presence')('1088814016035561532');
 const fs = require('fs');
 const results = [];
 
 const beautified_lesson_names = {
-    "wobi": "Meetup",
-    "de": "German",
-    "ge": "History",
-    "ma": "Math",
-    "Mittag": "Lunch",
-    "bio": "Biology",
-    "pb": "Political Science",
-    "reli": "Religion🔫",
-    "eng": "English",
-    "phy": "Physics",
-    "ku": "Art",
-    "semi": "Seminar Course",
-    "sp": "Sport",
-    "": "Break"
+  "wobi": "Meetup",
+  "de": "German",
+  "ge": "History",
+  "ma": "Math",
+  "Mittag": "Lunch",
+  "bio": "Biology",
+  "pb": "Political Science",
+  "reli": "Religion🔫",
+  "eng": "English",
+  "phy": "Physics",
+  "ku": "Art",
+  "semi": "Seminar Course",
+  "sp": "Sport",
+  "pause": "Break",
 }
 
 const date = new Date();
@@ -35,25 +35,30 @@ fs.createReadStream('timetable.csv')
       const [hour_n2, minute_n2] = time[1].split(':').map((x) => parseInt(x));
       if (hour_o > hour_n1 || (hour_o === hour_n1 && minute_o >= minute_n1)) {
         if (hour_o < hour_n2 || (hour_o === hour_n2 && minute_o <= minute_n2)) {
-            const lesson_start_time = new Date();
-            lesson_start_time.setHours(hour_n1, minute_n1, 0, 0);
+          const lesson_start_time = new Date();
+          lesson_start_time.setHours(hour_n1, minute_n1, 0, 0);
 
-            const lesson_end_time = new Date();
-            lesson_end_time.setHours(hour_n2, minute_n2, 0, 0);
-            
-            let current_status = "In school";
-            let current_lesson = results[i][day_name];
-            if (current_lesson) {
-                current_lesson = beautified_lesson_names[current_lesson];
-            }
+          const lesson_end_time = new Date();
+          lesson_end_time.setHours(hour_n2, minute_n2, 0, 0);
 
-            discord_rpc.updatePresence({
-                state: current_status,
-                details: "Currently in " + current_lesson,
-                startTimestamp: lesson_start_time,
-                endTimestamp: lesson_end_time,
-                instance: true,
-            });
+          let current_status = "In school";
+          let current_lesson = results[i][day_name];
+
+          if (current_lesson && beautified_lesson_names[current_lesson]) {
+            current_lesson = beautified_lesson_names[current_lesson];
+          }
+
+          if (!results[i] || results[i][day_name] == '') {
+            current_status = "Not in school"
+          }
+
+          discord_rpc.updatePresence({
+            state: "Currently in " + current_lesson,
+            details: current_status,
+            startTimestamp: lesson_start_time,
+            endTimestamp: lesson_end_time,
+            instance: false,
+          });
         }
       }
     }
